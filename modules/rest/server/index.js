693f4596d2a55e00fcea23985fcd4a1a5c983cf7
Kengessov <kschingiz@gmail.cm>
2019-12-13 19:17:50 +0600

    rest handler removed

--- modules/rest/server/index.js
+++ modules/rest/server/index.js
@@ -1,8 +1,6 @@
-import { Meteor } from "meteor/meteor";
 import { WebApp } from "meteor/webapp";
 import express from "express";
 import bodyParser from "body-parser";
-import MessageController from "./controllers/MessageController";
 
 const Rest = express();
 
@@ -10,7 +8,5 @@ Rest.use(bodyParser.json());
 Rest.use(bodyParser.urlencoded({ extended: true }));
 
 WebApp.connectHandlers.use(Rest);
-
-Rest.post("/api/message", Meteor.bindEnvironment(MessageController.sendMessage));
 
 export { Rest };
